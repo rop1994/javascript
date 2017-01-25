ed108ebcda8cd69cb18c57db23491cc48d3021c1
Roberto Perez <robertoperez@hotmail.com>
2017-01-26 00:31:22 +0100

    primer commit

---
+++ script.js
@@ -0,0 +1,42 @@
+//Variables
+/*var name = 'Roberto';
+console.log(name);
+
+var surname = 'Perez';
+console.log(surname);
+
+var age = 22 ;
+console.log(age);
+*/
+
+
+//Variables 2
+
+var name = 'Roberto';
+var age = 22;
+
+console.log(name + age);
+console.log(age + age);
+
+var job, car;
+
+job = 'driver';
+car = 'ferrari';
+
+/*
+console.log(job);
+console.log(car);
+*/
+
+console.log(' The name is ' + name + ' ' + age + ' years old ' + ',' + job + ' The car is ' + car);
+
+job = 'chef'
+car = 'fiat'
+
+console.log(' The name is ' + name + ' ' + age + ' years old ' + ',' + job + ' The car is ' + car);
+
+var city = prompt('Where are you from?');
+
+//console.log(' The name is ' + name + ' ' + age + ' years old ' + ',' + job + ' The car is ' + car + ' The city is ' + city  );   
+
+alert(' The name is ' + name + ' ' + age + ' years old ' + ',' + job + ' The car is ' + car + ' The city is ' + city)
